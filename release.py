e395e6fb3dbe011d50b10064695b9e05a3a8c529
adobe17 <601905450@qq.com>
2020-07-20 17:34:45 +0800

    V2.1发布版

--- release.py
+++ release.py
@@ -1,23 +1,37 @@
 #! py -3.5
 #-*- coding:utf-8 -*-
-
+from __future__ import print_function
 from random import sample
-from requests import get, post, session, exceptions
+from requests import get, post, session
+from requests.exceptions import Timeout, ReadTimeout, ConnectionError
 from re import compile
+from os import remove, mkdir
+from os.path import isfile, exists
 
+CONNECTION_TIME_OUT = 5
+READ_TIME_OUT = 10
 
-kb_File = './export/选课结果.html'
-yzm_File = './export/yzm.jpg'
-cjb_File = './export/成绩总表.html'
-bjg_File = './export/不及格成绩总表.html'
-css_File = './export/project.css'
+file_dir = './export/'
+kb_File = file_dir + '选课结果.html'
+yzm_File = file_dir + 'yzm.jpg'
+cjb_File = file_dir + '成绩总表.html'
+bjg_File = file_dir + '不及格成绩总表.html'
+css_File = file_dir + 'project.css'
 
+vpn_url = 'http://vpn.neau.edu.cn/do-login/'
 
-def setCookie(ori_cookie):
-    cookie_tmp = session()
-    for key, value in ori_cookie.items():
-        cookie_tmp.cookies.set(key, value)
-    return cookie_tmp.cookies
+lg_in = 'loginAction.do'
+lg_out = 'logout.do'
+
+xk = 'xkAction.do'
+tk = 'xkAction.do?actionType=10&kcId='
+kb = 'xkAction.do?actionType=6'
+cj = 'gradeLnAllAction.do?type=ln&oper=qbinfo'
+bjgcj = 'gradeLnAllAction.do?type=ln&oper=bjg'
+
+name = 'menu/top.jsp'
+yzm = 'validateCodeAction.do'
+css = 'css/newcss/project.css'
 
 
 proxy = {
@@ -49,7 +63,7 @@ login_params = {
     "dzslh": "",
     "zjh": "",
     "mm": "",
-    "v_yzm": ""
+    "v_yzm": "0"
 }
 xkcx_params = {
     'kch': '',		# 课程号
@@ -78,20 +92,12 @@ vpn_params = {
     # 'captcha_id': 'pnNauNa3WE6uG07'
 }
 
-vpn_url = 'http://vpn.neau.edu.cn/do-login/'
 
-lg_in = 'loginAction.do'
-lg_out = 'logout.do'
-
-xk = 'xkAction.do'
-tk = 'xkAction.do?actionType=10&kcId='
-kb = 'xkAction.do?actionType=6'
-cj = 'gradeLnAllAction.do?type=ln&oper=qbinfo'
-bjgcj = 'gradeLnAllAction.do?type=ln&oper=bjg'
-
-name = 'menu/top.jsp'
-yzm = 'validateCodeAction.do'
-css = 'css/newcss/project.css'
+def setCookie(ori_cookie):
+    cookie_tmp = session()
+    for key, value in ori_cookie.items():
+        cookie_tmp.cookies.set(key, value)
+    return cookie_tmp.cookies
 
 
 def randomSession(x):
@@ -112,21 +118,21 @@ def filter_str(cont, exp):
 
 def POST(url, cookie, debug, data={}):
     if(debug == 1):
-        return post(url, headers=header, data=data, cookies=cookie, timeout=(2, 4), proxies=proxy)
+        return post(url, headers=header, data=data, cookies=cookie, timeout=(CONNECTION_TIME_OUT, READ_TIME_OUT), proxies=proxy)
     else:
-        return post(url, headers=header, data=data, cookies=cookie, timeout=(2, 4))
+        return post(url, headers=header, data=data, cookies=cookie, timeout=(CONNECTION_TIME_OUT, READ_TIME_OUT))
 
 
 def GET(url, cookie, debug, data={}):
     if(debug == 1):
-        return get(url, headers=header, data=data, cookies=cookie, timeout=(2, 4), proxies=proxy)
+        return get(url, headers=header, data=data, cookies=cookie, timeout=(CONNECTION_TIME_OUT, READ_TIME_OUT), proxies=proxy)
     else:
-        return get(url, headers=header, data=data, cookies=cookie, timeout=(2, 4))
+        return get(url, headers=header, data=data, cookies=cookie, timeout=(CONNECTION_TIME_OUT, READ_TIME_OUT))
 
 
 def port1():
     choice = input(
-        '选择端口：\n1.jws1学生入口1\n2.jws2学生入口2\n3.jws3学生入口3\n4.jws4学生入口4\n\n5.jwt1教师入口1\n6.jwt2教师入口2\n7.jwt3教师入口3\n请输入序号：')
+        '选择端口：\n1.jws1学生入口1\n2.jws2学生入口2\n3.jws3学生入口3\n4.jws4学生入口4\n\n5.jwt1教师入口1\n6.jwt2教师入口2\n7.jwt3教师入口3\n\n请输入序号：')
     return url_port[int(choice)]
 
 
@@ -134,9 +140,26 @@ def port2(choice):
     return url_port[int(choice)]
 
 
+def get_yzm(debug=0):
+    try:
+        response = GET(url_selected + yzm, cookie, debug)
+        print('\n    【验证码】获取成功！\n')
+        file_write(yzm_File, response.content, 'wb', 0)
+        login_params['v_yzm'] = input("请打开yzm.jpg查看并输入验证码【如看不清或未成功获取请输入0】：")
+        if exists(yzm_File):
+            remove(yzm_File)
+        return 1
+    except Timeout or ConnectionError or ReadTimeout:
+        print('    【验证码】 请求超时！请重试或更换端口！')
+        return 0
+
+
 def get_css(debug=0):
-    response = GET(url_selected + css, cookie, debug)
-    file_write(css_File, response.text)
+    try:
+        response = GET(url_selected + css, cookie, debug)
+        file_write(css_File, response.text)
+    except Timeout or ConnectionError or ReadTimeout:
+        print('    【CSS】请求超时！用于输出网页带表格格式，非必要组件，可登陆后重试！')
 
 
 def name_check(debug=0):
@@ -147,7 +170,7 @@ def name_check(debug=0):
         if(result):
             print('\n    当前用户：'+result[0]+')  ')
             return '当前用户：'+result[0]+')'
-    except exceptions.Timeout:
+    except Timeout or ConnectionError or ReadTimeout:
         print('\n     【账户校验】请求超时！请重试或更换端口！')
 
 
@@ -185,7 +208,7 @@ def valid(debug=0):
         # with open(kb_File, 'w') as f:
         # 	f.write(response.text)
         # f.write(response.text.replace('charset=GBK', 'charset
-    except exceptions.Timeout:
+    except Timeout or ConnectionError or ReadTimeout:
         print('\n     【课表】请求超时！请重试或更换端口！\n')
 
 
@@ -213,12 +236,12 @@ def xk_do(debug=0):
                             print('\n  选课已执行！')
                         else:
                             print('\nxkAction.do连接失败！\n')
-                    except exceptions.Timeout:
+                    except Timeout or ConnectionError or ReadTimeout:
                         print('\n     【选课确认】请求超时！请重试或更换端口！\n')
                 else:
                     print('  500 Servlet Exception')
             valid(debug)
-    except exceptions.Timeout:
+    except Timeout or ConnectionError or ReadTimeout:
         print('\n     【选课】请求超时！请重试或更换端口！\n')
 
 
@@ -232,7 +255,7 @@ def tk_do(debug=0):
             valid()
         else:
             print('\n  连接失败！')
-    except exceptions.Timeout:
+    except Timeout or ConnectionError or ReadTimeout:
         print('\n     【退课】请求超时！请重试或更换端口！\n')
 
 
@@ -241,7 +264,7 @@ def kb_do(debug=0):
         response = GET(url_selected + kb, cookie, debug)
         file_write(kb_File, response.text.replace(
             '/css/newcss/project.css', './project.css'))
-    except exceptions.Timeout:
+    except Timeout or ConnectionError or ReadTimeout:
         print('\n     【课表】请求超时！请重试或更换端口！\n')
 
 
@@ -253,21 +276,25 @@ def cj_do(debug=0):
         response = GET(url_selected + bjgcj, cookie, debug)
         file_write(bjg_File, response.text.replace(
             '/css/newcss/project.css', './project.css'))
-    except exceptions.Timeout:
+    except Timeout or ConnectionError or ReadTimeout:
         print('\n     【成绩总表】请求超时！请重试或更换端口！\n')
 
 
 def login(cookie, debug=0):
-    try:
-        response = GET(url_selected + yzm, cookie, debug)
-        file_write(yzm_File, response.content, 'wb', 0)
-
-        jwc_password = input('请输入教务处密码：')
-
-        login_params['zjh'] = jwc_username
-        login_params['mm'] = jwc_password
-
-        login_params['v_yzm'] = input("请打开yzm.jpg查看并输入验证码：")
+    jwc_password = input('请输入教务处密码：')
+    login_params['zjh'] = jwc_username
+    login_params['mm'] = jwc_password
+    if not get_yzm(debug) or login_params['v_yzm'] == '0':
+        count = 1
+        while login_params['v_yzm'] == '0':
+            print('\n正在第 %d 次重新获取验证码...' % count)
+            if get_yzm(debug):
+                count = 0
+            count += 1
+            if(count > 4):
+                print('\n已经连续 5 次无法成功获取读取验证码！\n')
+                return 0
+    for i in range(3):
         try:
             response = POST(url_selected + lg_in, cookie, debug, login_params)
             # print(len(response.text))  8289  484
@@ -278,10 +305,10 @@ def login(cookie, debug=0):
             else:
                 print('\n登录成功！开始执行！\n')
                 return 1
-        except exceptions.Timeout:
-            print('\n    【教务处登录】 请求超时！请重试或更换端口！\n')
-    except exceptions.Timeout:
-        print('\n    【验证码】 请求超时！请重试或更换端口！\n')
+        except Timeout or ConnectionError or ReadTimeout:
+            print('\n    【教务处登录】 请求超时！正在重试！\n')
+
+    print('【教务处登录】重试 3 次超时！请尝试更换端口！（头铁可以继续）')
 
 
 def logout(cookie, debug=0):
@@ -289,7 +316,7 @@ def logout(cookie, debug=0):
         response = GET(url_selected + lg_out, cookie, debug)
         if(response.status_code == 200):
             print('\n已经登出当前账号！\n')
-    except exceptions.Timeout:
+    except Timeout or ConnectionError or ReadTimeout:
         print('\n    【教务处登出】 请求超时！请重试或更换端口！\n')
 
 
@@ -301,7 +328,7 @@ def vpn(debug=0):
         response = POST(vpn_url, cookie, debug,  vpn_params)
         # print(response.headers['Set-Cookie'])
         if response.headers['Content-Length'] != '35':
-            print('\nVPN登录失败！请检查账号密码或验证码是否输入错误！')
+            print('\nVPN登录失败！请检查账号密码或验证码是否输入错误！\n')
             return 0
         else:
             pattern = compile(r'cn=(.*); P')
@@ -309,14 +336,17 @@ def vpn(debug=0):
             cookie = setCookie(
                 {"wengine_vpn_ticketvpn_neau_edu_cn": result[0]})
             # print(cookie.items())
-            print('\n\nVPN登录成功！')
+            print('\nVPN登录成功！')
             return 1
-    except exceptions.Timeout:
+    except Timeout or ConnectionError or ReadTimeout:
         print('\n    【VPN登录】 请求超时！请重试或更换端口！\n')
 
-
 if __name__ == '__main__':
-
+    if exists(file_dir):
+        if isfile(file_dir):
+            mkdir(file_dir)
+    else:
+        mkdir(file_dir)
     debug_mode = int(input("127.0.0.1:8080代理模式(0/1)【不清楚有什么用的请选0】："))
     # debug_mode = 1
     vpncookie = {"wengine_vpn_ticketvpn_neau_edu_cn": randomSession(25)}
@@ -324,22 +354,18 @@ if __name__ == '__main__':
     vpncl = -1
     vpn_tries = 0
     jwc_tries = 0
-
     while(vpncl):
         if(vpn_tries >= 3):
             print(
-                '\n警告：VPN已连续 3 次尝试失败，为防止账户被封锁（连续五次登陆失败），脚本即将退出！\n为防止账户被锁，请仔细核对密码后重新登录！\n')
+                '\n警告：VPN已连续 3 次尝试失败，为防止账户被封锁（连续五次登陆失败），脚本即将退出！\n为防止账户被锁，请仔细核对密码后！\n')
             break
         vpn_tries += 1
-
         if vpn(debug_mode) == 1:
             tmp = setCookie({"JSESSIONID": randomSession(16)})
             cookie.update(tmp)
             vpn_tries = 0
-
             print('\n当前账户cookie：', cookie.items(), end='\n\n')
             url_selected = port2(7)
-
             while vpncl:
                 if vpncl == -1:
                     url_selected = port1()
@@ -347,25 +373,34 @@ if __name__ == '__main__':
                 if(jwc_tries >= 5):
                     print('\n教务处账号已连续 5 次尝试错误，请仔细核对后重新尝试！\n')
                     break
-
                 jwc_username = input('请输入教务处账号：')
-
                 if(login(cookie, debug_mode)):
                     get_css(debug_mode)
                     print('===================================')
-                    usrname = name_check(debug_mode)
+                    usr_tries = 1
+                    usrname = None
+                    while(usrname == None):
+                        print('正在尝试第 %d 次用户名校验...' % usr_tries)
+                        usrname = name_check(debug_mode)
+                        usr_tries += 1
+                        if usr_tries == 5:
+                            print("你这网也太差了吧……登录上了读不到用户名？？？")
                     print(
                         '\n注意cookie有效时间！\n建议每10分钟左右执行一次名字校验，如无返回结果，则证明cookie已过期，需退出当前账号重新登录！')
                     jwc_tries = 0
                     choose = 1
                     while(choose):
+
                         print('===================================')
-                        choose = int(
-                            input('%s\n\n1、选课  2、退课  3、查课表  4、成绩总表\n5、抢课模式【非公开项目】  6、名字校验（刷新cookie有效期）\n0、退出当前教务系统账号（退出后可更换端口）\n\n请输入选项：' % usrname))
+                        print('%s\n' % usrname)
+                        print('1、选课  2、退课  3、查课表  4、成绩总表')
+                        print('5、抢课模式  6、名字校验（刷新cookie有效期）')
+                        print('7、重新获取CSS')
+                        print('0、教务系统账号（退出后可更换端口）')
+                        choose = int(input('\n请输入选项：'))
+
                         if choose == 1 or choose == 2 or choose == 5:
-
                             kcId = input("请输入课程号：")
-
                             if choose == 1 or choose == 5:
                                 lkId = '_'
                                 kxId = input("请输入课序号：")
@@ -376,7 +411,6 @@ if __name__ == '__main__':
                                     xk_do(debug_mode)
                                 else:
                                     print('\n【抢课模式】功能不公开提供！\n')
-
                             elif choose == 2:
                                 tk_do(debug_mode)
                             print('\n请登录教务处核对课表！')
@@ -387,13 +421,18 @@ if __name__ == '__main__':
                             cj_do(debug_mode)
                             print('\n请以教务处成绩表为准！')
                         elif choose == 6:
-                            name_check(debug_mode)
-
+                            if usrname == None:
+                                usrname = name_check(debug_mode)
+                            else:
+                                name_check(debug_mode)
+                        elif choose == 7:
+                            get_css(debug_mode)
                         print(' \n执行完毕！')
                         print('***********************************')
                     logout(cookie, debug_mode)
                     print('-----------------------------------')
-                vpncl = int(input(
-                    '当前账号%s已登出/停止尝试，是否继续处理其他账号？\n(0 退出/ 1 处理其他账号/ -1 更换端口)\n' % (jwc_username)))
+                print('当前账号%s已登出/停止尝试，是否继续处理其他账号？' % (jwc_username))
+                vpncl = int(input('(0 退出/ 1 处理其他账号/ -1 更换端口)\n'))
     print('选课脚本已退出！感谢您的支持与使用！\n')
     print('如有任何修改建议或bug反馈请发送至：a17github@126.com 或 https://github.com/adobe17/NEAU-URP 的issue处提出')
+    input('Press AnyKey to Exit...')
